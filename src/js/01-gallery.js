454b23aca7b91a52bd3cfd13d0aba3e3d22e77e2
Крайняя Юлия <kraynya1991@gmail.com>
2022-02-07 19:30:54 +0200

    8 готово

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const pictureMarkup = createPictureMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', pictureMarkup);
+galleryContainer.addEventListener('click', onImageClick);
+
+
+function createPictureMarkup(pictures){
+  const markup =  pictures.map(({ description, original, preview } ) => {
+ return `
+ <a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+}).join("");
+  return markup;
+
+}
+
+function onImageClick(event) {
+  event.preventDefault();
+}
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData: `alt`,  captionDelay: 250});
+  
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
-
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Vimeo.Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+let currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+function getCurrentVideo(data) {
+    let timeInSeconds = data.seconds;
+    localStorage.setItem(LOCALSTORAGE_KEY, timeInSeconds);
+};
+player.on('timeupdate', throttle(getCurrentVideo, 1000));
+player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+        default:
+            // some other error occurred
+            break;
+    }
+});
